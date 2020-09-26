cb7e1f8ed9025f6ca4738a224978cf1af7f65922
Tischenko Sergiy <sytischenko@gmail.com>
2020-09-26 11:34:50 +0300

    Add files via upload

--- Lesson2/homework.py
+++ Lesson2/homework.py
@@ -1,10 +1,10 @@
 import argparse
 
 parser = argparse.ArgumentParser();
-parser.add_argument("first", default="Admin", help='First Name')
-parser.add_argument("last", default="Admin", help='Last Name')
+parser.add_argument("--first", default="Admin", help='First Name')
+ parser.add_argument("--last", default="Admin", help='Last Name')
 parser.add_argument("-p", "--pay", default=1000, dest='EPAY', metavar='PAY', type=int, help='Payment')
-parser.add_argument("--secret", help=argparse.SUPPRESS)
+    parser.add_argument("--secret", help=argparse.SUPPRESS)
 
 mgrp = parser.add_mutually_exclusive_group()
 mgrp.add_argument('--xml', action='store_true', help="Get xml data")
@@ -12,40 +12,46 @@ mgrp.add_argument('--html', action='store_true', help="Get html data")
 mgrp.add_argument('--yaml', action='store_true', help="Get yaml data")
 mgrp.add_argument('--json', action='store_true', help="Get json data")
 
-args = parser.parse_args()
+ args = parser.parse_args()
+
+
 # args.first, args.last, args.pay
 
 
 class Employee:
-    """A sample Employee class"""
+    """A sample Employee class."""
 
     raise_amt = 1.05
 
-    def __init__(self, first, last, pay):
+     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         print(self.__dict__)
 
-    @property
+     @property
     def email(self):
-         return '{}.{}@email.com'.format(self.first, self.last)
-
-   @property
-    def fullname(self):
-         return '{} {}'.format(self.first, self.last)
-
-    def apply_raise(self):
-        self.pay = int(self.pay * self.raise_amt)
-
-    def monthly_schedule(self, month):
-        #response = requests.get(f'http://company.com/{self.last}/{month}')
-        if response.ok:
-            return response.text
-        else:
-             return 'Bad Response!'
+        return '{}.{}@email.com'.format(self.first, self.last)
 
 
-if __name__ == "__main__":
+@property
+def fullname(self):
+    return '{} {}'.format(self.first, self.last)
+
+def apply_raise(self):
+    self.pay = int(self.pay * self.raise_amt)
+
+
+def monthly_schedule(self, month):
+    # response = requests.get(f'http://company.com/{self.last}/{month}')
+    # if response.ok:
+    #     return response.text
+    # else:
+    #     return 'Bad Response!'
+    pass
+
+# --- end of class definition ---
+
+ if __name__ == "__main__":
     first, last, pay = args.first, args.last, args.EPAY
     emp = Employee(first, last, pay)
